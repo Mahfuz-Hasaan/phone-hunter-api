0ed12f09f33890e96cae14b67acdc4ce24e1fce7
Mahfuz-Hasan <mahfuzhasan587@gmail.com>
2023-02-27 22:05:58 +0600

    input field added

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ showPhones = (phones) => {
     console.log(phone);
     const phoneChildContainer = document.createElement("div");
     phoneChildContainer.innerHTML = `
-        <div class="card w-96 bg-base-100 shadow-xl">
-        <figure class="px-10 pt-10">
+        <div class="card lg:w-96 bg-base-100 shadow-xl bg-sky-500 ">
+        <figure class="px-10 pt-10 ">
           <img src="${phone.image}" />
         </figure>
         <div class="card-body items-center text-center">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
+            <button class="btn btn-primary hover:bg-purple-900">Buy Now</button>
           </div>
         </div>
       </div>
